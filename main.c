fe3b645f40a359b7853ee533bf6650c3c6b5667e
kenialo19 <3396@holbertonschool.com>
2021-09-21 22:34:15 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+/**
+ * main -Monty bytecodes files interpreter
+ * @argc: total of arguments
+ * @argv: The arguments, monty files
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		printf("holi\n");
+		/**error_argv();
+	op_re_file(argv);*/
+	return (0);
+}
